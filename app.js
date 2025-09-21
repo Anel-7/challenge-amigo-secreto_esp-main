da7c77c8de930a464739a4dc116681c9b9dd6c71
Anel-7 <anelstudent@gmail.com>
2025-09-21 13:29:30 -0500

    refresh changes

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ let amigos = [];
 function agregarAmigo(){
     let inputAmigo = document.getElementById("amigo");
     let amigo = inputAmigo.value;
-
+    console.log(amigo);
     amigos.push(amigo);
     console.log(amigos);
 }
