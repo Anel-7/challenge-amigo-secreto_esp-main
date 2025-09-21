88eba211efea07b8f02a3d77b495a1a50833b209
Anel-7 <anelstudent@gmail.com>
2025-09-21 11:15:03 -0500

    init

---
+++ app.js
@@ -0,0 +1,10 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo(){
+    let inputAmigo = document.getElementById("amigo");
+    let amigo = inputAmigo.value;
+
+    amigos.push(amigo);
+    console.log(amigos);
+}
