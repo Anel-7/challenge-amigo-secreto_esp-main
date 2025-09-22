5ccba3b9714904dddb74498b6cf8260ee1d1a750
Anel-7 <anelstudent@gmail.com>
2025-09-22 00:16:11 -0500

    refresh chamge

--- app.js
+++ app.js
@@ -4,7 +4,49 @@ let amigos = [];
 function agregarAmigo(){
     let inputAmigo = document.getElementById("amigo");
     let amigo = inputAmigo.value;
-    console.log(amigo);
+    console.log(typeof amigo);
+    
+    if(amigo === ""){
+        alert("Por favor, ingrese el nombre de un amigo");
+        return;
+    } else if(amigo === " "){
+        alert("Por favor, ingrese el nombre de un amigo");
+        return;
+    } else if(amigo === "  "){
+        alert("Por favor, ingrese el nombre de un amigo");
+        return;
+    }
+
+    limpiarCaja();
     amigos.push(amigo);
     console.log(amigos);
+
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+    amigos.forEach(nombre => {
+        const li = document.createElement("li");
+        li.textContent = nombre;
+        lista.appendChild(li);
+    });
 }
+
+function limpiarCaja(){
+    let valorCaja = document.querySelector("#amigo");
+    valorCaja.value = "";
+}
+
+function sortearAmigo(){
+    const nombreAleatorio = Math.floor(Math.random()*amigos.length);
+    const resultado = `El nombre sorteado es: ${amigos[nombreAleatorio]}`
+    console.log(`El nombre sorteado es: ${resultado}`);
+
+    let nombreSorteado = document.getElementById("resultado");
+    nombreSorteado.innerHTML = "";
+    const li = document.createElement("li");
+    li.textContent = resultado;
+    nombreSorteado.appendChild(li);
+
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+}
+
